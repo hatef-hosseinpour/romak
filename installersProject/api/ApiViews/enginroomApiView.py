a593cc5b4193cb65c9477fc76660fa669e125728
Hatef Hosseinpour <hatef.hosseinpour@outlook.com>
2023-08-23 11:15:20 +0330

    Add captcha

--- installersProject/api/ApiViews/enginroomApiView.py
+++ installersProject/api/ApiViews/enginroomApiView.py
@@ -1,9 +1,7 @@
 from rest_framework import status, authentication, permissions, viewsets
 from rest_framework.response import Response
 from main.models import Enginroom, locationPublicInfo
-from api.serializers import EnginroomSerializers, LocationPublicInfoSerializers
-
-
+from api.serializers import EnginroomSerializers
 
 class EnginroomViewSet(viewsets.ModelViewSet):
     authentication_classes = [authentication.SessionAuthentication]
